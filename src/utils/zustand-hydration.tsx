3c672a382ef92b60a7ca4aa696d17f38207e979d
danishweb <danishshaikh5121@gmail.com>
2023-12-14 07:41:42 +0530

    Fix hydration issues for zustand

---
+++ src/utils/zustand-hydration.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+const HydrationZustand = ({ children }: { children: React.ReactNode }) => {
+  const [isHydrated, setIsHydrated] = useState(false);
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
+  return <>{isHydrated ? <div>{children}</div> : null}</>;
+};
+
+export default HydrationZustand;
